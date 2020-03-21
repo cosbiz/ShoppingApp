95b13d725b1c6dc87fe5c84e73b5a52a82ea2376
Cosmin <cosminstudio@gmail.com>
2020-03-21 14:25:00 +0200

    update 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -17,7 +16,6 @@ import { RecipesModule } from './recipes/recipes.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     SharedModule,
